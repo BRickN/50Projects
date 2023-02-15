f03bb14d93537b5069f9968d232fd2346ffc44d1
bmla.nieling@student.han.nl <rick.niel@hotmail.com>
2023-02-15 18:10:16 +0100

    day 3 - rotating navigation

---
+++ Day3RotatingNavigation/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    const open = document.getElementById('open');
+    const close = document.getElementById('close');
+    const container = document.querySelector('.container');
+
+    open.addEventListener('click', function(){
+        container.classList.add('show-nav')
+    })
+    close.addEventListener('click', function(){
+        container.classList.remove('show-nav')
+    })
+}
+
+
+
