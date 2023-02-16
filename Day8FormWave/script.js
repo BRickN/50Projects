552181cecc5b32776965fe4d3209cfa115f5681d
Rick Nieling <rick.nieling@perplex.nl>
2023-02-16 16:41:41 +0100

    day 8 form-wave

---
+++ Day8FormWave/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+//    const labels = document.querySelectorAll('.form-control label')
+//    labels.forEach(function(label){
+//     label.innerHTML = label.innerText
+//     .split('')
+//     .map((x, idx) => `<span>${x}</span>`)
+//     .join('')
+//    })
+}
