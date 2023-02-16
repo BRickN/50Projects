606f8d7811826c1d82f6519962a1560a9299f30f
Rick Nieling <rick.nieling@perplex.nl>
2023-02-16 14:51:02 +0100

    day 6 scrolling animation

---
+++ Day6ScrollingAnimation/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    const boxes = document.querySelectorAll('.box');
+    checkBoxes();
+
+    window.addEventListener('scroll', checkBoxes);
+
+    function checkBoxes() {
+        const triggerBottom = window.innerHeight / 5 * 4;
+
+        boxes.forEach(function(box){
+            const boxTop = box.getBoundingClientRect().top;
+
+            if(boxTop < triggerBottom){
+                box.classList.add('show')
+            } else{
+                box.classList.remove('show')
+            }
+        });
+    }
+}
